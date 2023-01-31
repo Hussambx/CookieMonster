a9d786c1c0b34f70c3fc64b83362cfaf5256a050
ToxicTerror21 <hussam.baradei@yahoo.com>
2023-01-31 10:58:03 -0500

    Confirmation UI:Add to bag/Upgrade Box/Quantity

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,14 +7,15 @@ import cookieboxdata from './data/cookieboxsizes'
 import cookietypes from './data/cookieoptions'
 import Cookiemenu from './components/Cookiemenu'
 import Boxlayout from './components/Boxlayout'
+import Confirmselection from './components/Confirmselection'
 function App() {
   const [activeui, setUi] = useState(0);  //Zero =Main Menu, 1=Secondary Menu, 2 = Cookie Menu, 3=Drink Screen 
   const [boxsize,setSize] = useState(1);  //Keeps track of selected cookie box sizes, Options: 1,4,6,12
   const[cookielayout,setLayout] = useState([]); //Keeps track of the cookie box layout (Which cookies are in the given box + Order Of Them Left To Right),  this is only temp and once layout is confirmed list will be pushed to cart 
   const [drawerState, setDrawerState] = useState(false);   //This is used to force re-render Boxlayout element 
-  
+  const[cart,setCart] = useState([]); //This state keeps track of the persons cart(Items that they have addded to the order so far )
   var tempclone;
-  
+  var tempclone2;
   //Keeps track of what UI element should be displayed 
   function changeview(num){
     setUi(prevstate=>1)
@@ -25,10 +26,10 @@ function App() {
   function cookieboxsize(num,name,price){
     setUi(prevstate=>2)
     setSize(prevstate=>num)
-    var a= [{name:name,price:price,index:0}] //Sets the name of the box + price 
+    var a= [{name:name,price:price,index:0,quantity:1}] //Sets the name of the box + price 
     setLayout(prevstate=>a); 
   }
-
+  console.log(cart);
   
   //Is Called when a cookie flavor is selected, adds the given cookie to the box 
   function pickedcookie(name,img,id){
@@ -52,7 +53,32 @@ function App() {
     setLayout((prevstate)=>
     prevstate.filter((cookiex)=>cookiex.index!=index)
     );
+    setDrawerState(!drawerState); //Force Rerender 
+    console.log(cookielayout)
     }
+
+    //This function is called when a customer has confirmed there selection, the selection is then added to the customers cart 
+    function addtocart(quantityx){
+      //Update the quanity field to the given quanity by the end of the counter, will then push cookielayout state into cart state
+      if(activeui==2){
+        tempclone = cookielayout
+        tempclone[0].quantity = quantityx
+        setLayout(prevstate=>tempclone)
+        console.log(cookielayout)
+        tempclone2 = cart;
+        tempclone2.push(tempclone)
+        setCart(prevstate=>tempclone2)
+        setLayout(prevstate=>[])
+        setUi(prevstate=>0)
+      }
+    }
+
+    //This function is called when a customer has hit the upgrade box option, this function only applies for cookies not DRINKS
+    function upgradebox(){
+      //Changes box size from 1->4 , 4->6, 6->12
+      boxsize==1?setSize(prevstate=>4):boxsize==4?setSize(prevstate=>6):setSize(prevstate=>12)
+    }
+
 
   //Props Compo for the cookie box types (sizes)
   const cookiebox =cookieboxdata.map(cookieboxdata=>{
@@ -93,7 +119,8 @@ function App() {
     <div id="rows">
     {activeui ==1 && cookiebox}
       </div>
-      {activeui==2 && <h2 id='largetext'>Pick Cookies For Your Box</h2>}
+      {activeui==2 &&  boxsize-cookielayout.length+1>0 && <h2 id='largetext'>Pick Cookies For Your Box</h2>}
+      {activeui==2 &&  boxsize-cookielayout.length+1==0 &&<h2 id='largetext'>How does this look?</h2>}
       {activeui==2 && <h5 id='desc'>Tap a cookie to remove from box</h5>}
       {activeui==2 && <Boxlayout
        size={boxsize}
@@ -103,11 +130,13 @@ function App() {
       />}
         {activeui==2 && boxsize-cookielayout.length+1>0&& <h5 id='desc'>{boxsize-cookielayout.length+1} left</h5>}
       <div id="cookieoption">
-        
-      {activeui==2 && cookieoptions}
-        
+      {activeui==2 && boxsize-cookielayout.length+1>0 &&cookieoptions}
         </div>
-
+        { boxsize-cookielayout.length+1==0 &&<Confirmselection
+        tocart={addtocart} upgrade={upgradebox} sizeofbox={boxsize}
+        
+        
+        />}
      
   
     </>
---
+++ frontend/src/components/Confirmselection.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { useState } from "react";
+export default function Confirmselection(props){
+
+    //This screen allows customer to adjust quanity, upgrade box and add the order to their bag 
+    //Only rendered when the customer has either fully filled there cookie box or they are on the drink selection screen 
+
+    //Counter Element keeps track of quantity
+    const[counter,setCount] =useState(1);
+
+    function addCount(){
+        if(counter<12){
+            setCount(prevstate=> counter+1)
+        }  
+    }
+    function removeCount(){
+        if(counter>1){
+            setCount(prevstate=>counter-1)
+        }
+    }
+    //Function is called when customer hits the add to bag option 
+    function addtocart(){
+        props.tocart(counter)
+    }
+    
+    //Function is called when customer hits upgrade box option 
+    function upgradebox(){
+    props.upgrade()
+    }
+
+
+    return(
+        <>
+        <h5>Quantity </h5>
+        <div className="counter">
+        <button className="dot" onClick={removeCount} >-</button>
+        <h5>{counter}</h5>
+        <button className="dot" onClick={addCount}>+</button>
+        </div>
+        <div className="counter"> 
+      {props.sizeofbox!=12 &&  <button className="addtocart" onClick={upgradebox}>UPGRADE BOX</button>}
+        <button className="addtocart" onClick={addtocart}>ADD TO BAG</button>
+        
+        </div>
+       
+      
+        </>
+    )
+}
